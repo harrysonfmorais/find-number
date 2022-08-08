5d038115279d9353764bbe76ef160a4673177cf3
Harryson Morais <harrysonfmorais@gmail.com>
2022-08-08 07:02:04 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+//variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+//eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter') {
+    handleResetClick()
+  }
+})
+
+//funções
+function handleTryClick(event) {
+  event.preventDefault()
+  
+  const inputNumber = document.querySelector("#inputNumber")
+
+  if(Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas!`
+  }
+  inputNumber.value = ""
+  xAttempts++
+}
+
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+}
+
+function handleEnterClick(e) {
+  if(e.key == 'Enter') {
+    handleResetClick()
+  }
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
